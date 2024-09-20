394ae53dabd4ea5b5f4cb01c676a57cddb87873a
gigg88 <popp.christian88@gmail.com>
2024-09-20 08:45:22 +0300

    standard setup done

---
+++ main.js
@@ -0,0 +1,10 @@
+//1.Create an html-page for displaying and editing text. When opening the page, the text should be displayed with div tag.
+// When clicking Ctrl+E, textarea appears instead of div and can be edited. When pressing Ctrl+S, the div with edited text appears instead of textarea.
+// Don’t forget to turn off default settings for these hotkey combinations.
+
+//2.Create an html-page with a large table. When clicking the column heading, it is necessary to sort data of that column.
+// Note that numerical values should be sorted as numbers, not as strings.
+
+//3.Create an html-page with a text block in a frame. Execute the possibility to change the size of the block when holding the mouse
+// cursor in the bottom right corner and pull it further.
+
